55ee14028bb244e6779f86a855adca88b6f3e7bf
Ibuki Aida <ibuki.b.2.5@gmail.com>
2025-03-16 22:20:13 +0900

    [update]画面変更いぶき

--- src/components/organisms/deatilNameContainer.tsx
+++ src/components/organisms/deatilNameContainer.tsx
@@ -5,6 +5,7 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import Box from '@mui/material/Box';
 
 export default function AccordionExpandIcon() {
   return (
@@ -14,10 +15,23 @@ export default function AccordionExpandIcon() {
           expandIcon={<ArrowDownwardIcon />}
           aria-controls="panel1-content"
           id="panel1-header"
+          sx={{ display: "flex", alignItems: "center" }} // 高さを揃える
         >
-          <Typography variant="h5" component="span" sx={{ fontWeight: 'bold' }}>
-            オンボーディング支援 : 業務知識・手続き支援エージェント
-          </Typography>
+          <Box
+          sx={{
+          backgroundColor: "#E3823D",
+          color: "#fff",
+          borderRadius: "12px",
+          padding: "6px 12px", // 少し余白を調整
+          display: "flex", // 高さを統一
+          alignItems: "center",
+          fontSize: "1.5rem",
+          fontWeight: "bold",
+          marginRight: "8px",
+        }}
+      >オンボーディング支援</Box>
+          <Typography variant='h5' component="span" sx={{ display: "flex", alignItems: "center", fontWeight: "bold" }}>  業務知識・手続き支援エージェント</Typography>
+
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
