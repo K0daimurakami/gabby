efd244b26e0f670a9d494967344c3d99732e9574
kodaimurakami <k31m03.1020@gmail.com>
2025-03-16 15:33:58 +0900

    Initial commit

--- src/pages/details/detailsSlice.ts
+++ src/pages/details/detailsSlice.ts
@@ -1,22 +1,72 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
+interface Message {
+  id: string;
+  role: "user" | "bot";
+  userName: string;
+  text: string;
+}
+
+interface ProcessMessage {
+  id: string;
+  state: "processing" | "success" | "error";
+  isloading: boolean;
+  text: string;
+}
+
 interface DetailsState {
-  texts: string[];
+  messages: Message[];
+  onProcessing: boolean;
+  isActiveSendButton: boolean;
+  processMessages: ProcessMessage[];
+  isShowOutput: boolean;
 }
 
 const initialState: DetailsState = {
-  texts: [],
+  messages: [],
+  onProcessing: false,
+  isActiveSendButton: false,
+  processMessages: [],
+  isShowOutput: false,
 };
 
 const detailsSlice = createSlice({
   name: "details",
   initialState,
   reducers: {
-    addText: (state, action: PayloadAction<string>) => {
-      state.texts.push(action.payload);
+    sendMessage: (state, action: PayloadAction<Message>) => {
+      state.messages.push({
+        ...action.payload,
+      });
+      state.onProcessing = true;
+    },
+    endProcess: (state) => {
+      state.onProcessing = false;
+    },
+    activeSendButton: (state) => {
+      state.isActiveSendButton = true;
+    },
+    deactiveSendButton: (state) => {
+      state.isActiveSendButton = false;
+    },
+    receiveMessage: (state, action: PayloadAction<Message>) => {
+      state.messages.push(action.payload);
+    },
+    clearProcessMessages: (state) => {
+      state.processMessages = [];
+      state.onProcessing = false;
+      state.isActiveSendButton = false;
     },
   },
 });
 
-export const { addText } = detailsSlice.actions;
+export const {
+  sendMessage,
+  endProcess,
+  activeSendButton,
+  deactiveSendButton,
+  receiveMessage,
+  clearProcessMessages,
+} = detailsSlice.actions;
+
 export default detailsSlice.reducer;
