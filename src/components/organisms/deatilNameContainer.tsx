1b5c6b7e420c9d89b290561a4f0f9bc458d44375
gxdxgx <ikemen.engineer@gmail.com>
2025-03-16 08:36:56 +0000

    saikyo

---
+++ src/components/organisms/deatilNameContainer.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+
+export default function AccordionExpandIcon() {
+  return (
+    <div>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ArrowDownwardIcon />}
+          aria-controls="panel1-content"
+          id="panel1-header"
+        >
+          <Typography component="span">オンボーディング支援 : 業務知識・手続き支援エージェント</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            新入社員への業務知識の提供と必要な手続きをガイドします。
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
+}
